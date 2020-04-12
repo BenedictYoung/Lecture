e25fcbcd99f00f66a68ed4682667aad9639b35d2
炉灰 <33519163+BenedictYoung@users.noreply.github.com>
2020-04-12 22:32:00 +0800

    Upload Chapter1

---
+++ Chapter1/1.1.cpp
@@ -0,0 +1,19 @@
+/*
+* 题目名称：A + B
+* 题目来源：HDU 1089
+* 题目链接：http://acm.hdu.edu.cn/showproblem.php?pid=1089
+* 代码作者：杨泽邦(炉灰)
+*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    while (scanf("%d%d", &a, &b) != EOF) {
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
