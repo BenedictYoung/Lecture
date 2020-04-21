4ccaa98b0bf22dd6231ec760a703a4c8f0ab7860
炉灰 <33519163+BenedictYoung@users.noreply.github.com>
2020-04-21 09:21:57 +0800

    Upload Chapter11

--- Chapter11/11.2.cpp
+++ Chapter11/11.2.cpp
@@ -12,27 +12,27 @@ using namespace std;
 
 const int MAXN = 1000 + 10;
 
-int father[MAXN];                       		//父亲结点
-int height[MAXN];                         		//结点高度
+int father[MAXN];                               //父亲结点
+int height[MAXN];                               //结点高度
 
-void Initial(int n) {                   		//初始化
+void Initial(int n) {                           //初始化
     for (int i = 0; i <= n; i++) {
-        father[i] = i;                  		//每个结点的父亲为自己
-        height[i] = 0;                    		//每个结点的高度为零
+        father[i] = i;                          //每个结点的父亲为自己
+        height[i] = 0;                          //每个结点的高度为零
     }
 }
 
-int Find(int x) {                       		//查找根结点
-    if (x != father[x]) {               		//路径压缩
+int Find(int x) {                               //查找根结点
+    if (x != father[x]) {                       //路径压缩
         father[x] = Find(father[x]);
     }
     return father[x];
 }
 
-void Union(int x, int y) {           			//合并集合
+void Union(int x, int y) {                      //合并集合
     x = Find(x);
     y = Find(y);
-    if (x != y) {                       		//矮树作为高树的子树
+    if (x != y) {                               //矮树作为高树的子树
         if (height[x] < height[y]) {
             father[x] = y;
         } else if (height[y] < height[x]) {
@@ -51,16 +51,16 @@ int main() {
         if (n == 0 && m == 0) {
             break;
         }
-        Initial(n);                     		//初始化
+        Initial(n);                             //初始化
         while (m--) {
             int x, y;
             scanf("%d", &x);
             scanf("%d", &y);
-            Union(x, y);                		//合并集合
+            Union(x, y);                        //合并集合
         }
-        int component = 0;              		//连通分量
+        int component = 0;                      //连通分量
         for (int i = 1; i <= n; i++) {
-            if (Find(i) == i) {         		//集合数目
+            if (Find(i) == i) {                 //集合数目
                 component++;
             }
         }
