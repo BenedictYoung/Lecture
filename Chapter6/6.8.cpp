eb4ae007aa28caf9fa1e8042020b7537a6ffee3b
炉灰 <33519163+BenedictYoung@users.noreply.github.com>
2020-04-24 20:29:35 +0800

    Upload Chapter6

--- Chapter6/6.8.cpp
+++ Chapter6/6.8.cpp
@@ -13,17 +13,17 @@ using namespace std;
 
 const int MAXN = 10000 + 10;
 
-vector<int> prime;                       		//保存质数
-bool isPrime[MAXN];                  			//标记数组
+vector<int> prime;                              //保存质数
+bool isPrime[MAXN];                             //标记数组
 
 void Initial() {
-    for (int i = 0; i < MAXN; ++i) {			//初始化
+    for (int i = 0; i < MAXN; ++i) {            //初始化
         isPrime[i] = true;
     }
     isPrime[0] = false;
     isPrime[1] = false;
     for (int i = 2; i < MAXN; ++i) {
-        if (!isPrime[i]) {             			//非质数则跳过
+        if (!isPrime[i]) {                      //非质数则跳过
             continue;
         }
         prime.push_back(i);
@@ -31,7 +31,7 @@ void Initial() {
             continue;
         }
         for (int j = i * i; j < MAXN; j += i) {
-            isPrime[j] = false;        			//质数的倍数为非质数
+            isPrime[j] = false;                 //质数的倍数为非质数
         }
     }
     return ;
@@ -41,7 +41,7 @@ int main() {
     Initial();
     int n;
     while (scanf("%d", &n) != EOF) {
-        bool isOutput = false;        			//判断是否有输出
+        bool isOutput = false;                  //判断是否有输出
         for (int i = 0; i < prime.size() && prime[i] < n; ++i) {
             if (prime[i] % 10 == 1) {
                 isOutput = true;
