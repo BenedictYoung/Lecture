c9413ca0771335a70375bb7e43e6069486b1c87a
炉灰 <yzb19@mails.tsinghua.edu.cn>
2021-01-28 11:45:05 +0800

    Update 5.6.cpp

--- Chapter5/5.6.cpp
+++ Chapter5/5.6.cpp
@@ -54,31 +54,31 @@ int main() {
         if (str == "0") {
             break;
         }
-        stack<char> oper;                   //运算符栈
-        stack<double> data;                 //运算数栈
+        stack<char> operation;              //运算符栈
+        stack<double> number;               //运算数栈
         str += '$';                         //字符串尾部添加$
-        oper.push('#');                     //运算符栈底添加#
+        operation.push('#');                //运算符栈底添加#
         int index = 0;                      //字符串下标
         while (index < str.size()) {
             if (str[index] == ' ') {
                 index++;
             } else if (isdigit(str[index])) {
-                data.push(GetNumber(str, index));
+                number.push(GetNumber(str, index));
             } else {
-                if (Priority(oper.top()) < Priority(str[index])) {
-                    oper.push(str[index]);
+                if (Priority(operation.top()) < Priority(str[index])) {
+                    operation.push(str[index]);
                     index++;
                 } else {
-                    double y = data.top();
-                    data.pop();
-                    double x = data.top();
-                    data.pop();
-                    data.push(Calculate(x, y, oper.top()));
-                    oper.pop();
+                    double y = number.top();
+                    number.pop();
+                    double x = number.top();
+                    number.pop();
+                    number.push(Calculate(x, y, operation.top()));
+                    operation.pop();
                 }
             }
         }
-        printf("%.2f\n", data.top());
+        printf("%.2f\n", number.top());
     }
     return 0;
 }
