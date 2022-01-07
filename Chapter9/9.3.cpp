77fe4402533b11fd28f614bf65869d7c255359d5
杨泽邦 <yzb19@mails.tsinghua.edu.cn>
2022-01-07 13:42:54 +0800

    Update 9.3.cpp

--- Chapter9/9.3.cpp
+++ Chapter9/9.3.cpp
@@ -24,21 +24,20 @@ bool DFS(int x, int y, int step, string answer, int p, int q) {
     if (step == p * q) {                        //搜索成功
         cout << answer << endl << endl;
         return true;
-    } else {
-        for (int i = 0; i < 8; ++i) {           //遍历邻居结点
-            int nx = x + direction[i][0];       //扩展状态坐标
-            int ny = y + direction[i][1];
-            if (nx < 0 || nx >= p || ny < 0 || ny >= q || visit[nx][ny]) {
-                continue;
-            }
-            char col = ny + 'A';                //该点编号
-            char row = nx + '1';
-            visit[nx][ny] = true;               //标记该点
-            if (DFS(nx, ny, step + 1, answer + col + row, p, q)) {
-                return true;
-            }
-            visit[nx][ny] = false;              //取消标记
+    }
+    for (int i = 0; i < 8; ++i) {           //遍历邻居结点
+        int nx = x + direction[i][0];       //扩展状态坐标
+        int ny = y + direction[i][1];
+        if (nx < 0 || nx >= p || ny < 0 || ny >= q || visit[nx][ny]) {
+            continue;
         }
+        char col = ny + 'A';                //该点编号
+        char row = nx + '1';
+        visit[nx][ny] = true;               //标记该点
+        if (DFS(nx, ny, step + 1, answer + col + row, p, q)) {
+            return true;
+        }
+        visit[nx][ny] = false;              //取消标记
     }
     return false;
 }
